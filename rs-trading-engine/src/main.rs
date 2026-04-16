9ba1465ced609992215ce1751687ea4ac4740221
thutasann <thutasann2002@gmail.com>
2026-04-16 23:37:20 +0800

    add(rs-trading): init

--- rs-trading-engine/src/main.rs
+++ rs-trading-engine/src/main.rs
@@ -1,3 +1,45 @@
+enum BidOrAsk {
+  Bid,
+  Ask,
+}
+
+#[derive(Debug)]
+struct Price {
+  integral: u64,
+  fractional: u64,
+  scalar: u64,
+}
+
+impl Price {
+  fn new(price: f64) -> Price {
+    let scalar = 100000;
+    let integral = price as u64;
+    let fractional = ((price % 1.0) * scalar as f64) as u64;
+    Price {
+      scalar,
+      integral,
+      fractional,
+    }
+  }
+}
+
+struct Limit {
+  price: Price,
+  orders: Vec<Order>,
+}
+
+struct Order {
+  size: f64,
+  bid_or_ask: BidOrAsk,
+}
+
+impl Order {
+  fn new(bid_or_ask: BidOrAsk, size: f64) -> Order {
+    Order { size, bid_or_ask }
+  }
+}
+
 fn main() {
-  println!("Hello, world!");
+  let price = Price::new(50.5);
+  println!("{:?}", price)
 }
